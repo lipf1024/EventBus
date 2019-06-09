c5136101bde059e539441002170a648064668ede
busytime <905937209@qq.com>
2019-06-09 17:22:29 +0800

    修复依赖冲突问题

--- app/src/main/java/com/my/one/eventbus/MainActivity.kt
+++ app/src/main/java/com/my/one/eventbus/MainActivity.kt
@@ -2,13 +2,18 @@ package com.my.one.eventbus
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.eventbus.one.eventbus.inf.Bus
+import com.eventbus.one.eventbus.inf.Message
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Bus.bus
+        Bus.bus.register("MMM"){r->
+            Log.d("QQQQQ",r.body())
+        }
+        Bus.bus.sent("MMM", Message.creat().setMsg("asdnxccncj"))
     }
 }
